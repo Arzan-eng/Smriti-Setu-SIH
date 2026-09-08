4241f2d35bf026dd3687524c881d8c4a1f1795af
Arzan-eng <arzantamboli@gmail.com>
2026-09-08 21:48:42 +0530

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Smriti Setu SIH</h1>
+        <p>Welcome to the app!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
